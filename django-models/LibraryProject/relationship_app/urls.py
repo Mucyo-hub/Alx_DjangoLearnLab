864a086ba55e4d7c3e2a73a42a38f88127e86cdc
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-02-23 15:58:46 +0200

    Added root URL redirection to book list page

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # Function-based view for listing books
-    path('books/', views.list_books, name='list_books'),
-    # Class-based view for displaying library details
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     # Authentication URLs
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
