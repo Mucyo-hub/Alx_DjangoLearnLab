185d4de08f657684fb9a139e05308cada03cf276
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-02 18:15:15 +0200

    Implemented Django security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponseForbidden
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required , permission_required
 from .models import Book
+from django.db.models import Q
+from .forms import BookForm # type: ignore
 
 # View all books (Requires "can_view" permission)
+@login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
 # Create a new book (Requires "can_create" permission)
+@login_required
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == "POST":
@@ -20,6 +24,7 @@ def create_book(request):
     return render(request, 'bookshelf/create_book.html')
 
 # Edit a book (Requires "can_edit" permission)
+@login_required
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
@@ -31,6 +36,7 @@ def edit_book(request, book_id):
     return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 # Delete a book (Requires "can_delete" permission)
+@login_required
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
