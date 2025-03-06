6c75ed392b4dc3273817a983b1e8ccf21754c933
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-06 10:35:13 +0200

    Added BookViewSet with CRUD operations using Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# full CRUD functionality
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
