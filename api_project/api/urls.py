c39d7c78d93fec1680b5bba8b7e4da2569d9ed7c
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-06 10:56:03 +0200

    Implemented Token Authentication and Permission Classes for API Security

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path,include 
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookList,BookViewSet
 
@@ -9,4 +10,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),  # Automatically includes all routes from the router
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token retrieval endpoint
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
 from rest_framework import generics,viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Require authentication for this view
+
 
 # full CRUD functionality
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
