aedae7efb3b08f6d3a372278232391eb2f24e0b9
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-13 12:58:57 +0200

    Adding line from django_filters import rest_framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters import rest_framework
 from rest_framework import generics, filters
 
 from django_filters.rest_framework import DjangoFilterBackend
