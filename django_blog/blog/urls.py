a21e0c7768dee794a5acc4eaebf6114a38a10da8
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-20 12:30:31 +0200

    Implement CRUD functionality for blog posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),  # List view
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),  # Detail view
     path('post/new/', PostCreateView.as_view(), name='post_create'),  # Create view
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),  # Update view
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),  # Update view
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),  # Delete view
 ]
