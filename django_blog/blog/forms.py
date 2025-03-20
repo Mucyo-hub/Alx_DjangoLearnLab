c5d18d9dfe7514898856198bf3730759aaea9518
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-20 15:41:48 +0200

    Add TagWidget to PostForm for tagging functionality in blog/forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget  # Import the TagWidget
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -16,6 +17,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content','tags']
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for the tags field
+        }
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
