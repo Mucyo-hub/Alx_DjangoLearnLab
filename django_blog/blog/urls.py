9d24248a3abd8bef3bec4818e5945129f1da0266
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-03-20 12:18:06 +0200

    Correcting PostUpdateView path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),  # Update view  # type: ignore
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
